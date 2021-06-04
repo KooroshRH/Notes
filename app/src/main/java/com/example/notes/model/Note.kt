a8e437c2b4612fc973717464aae71282919dcc40
Korosh <korosh.roohi9731@gmail.com>
2021-06-04 19:08:52 +0430

    Make some changes in note model and also add to one relation to it.

--- app/src/main/java/com/example/notes/model/Note.kt
+++ app/src/main/java/com/example/notes/model/Note.kt
@@ -2,11 +2,15 @@ package com.example.notes.model
 
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import io.objectbox.relation.ToOne
 
 @Entity
 data class Note (
     @Id
     var id: Long = 0,
     private var title: String? = null,
-    private var text: String? = null
-)
+    private var text: String? = null,
+    private var lastTimeModified: Int = 0
+) {
+    lateinit var folder: ToOne<Folder>
+}
